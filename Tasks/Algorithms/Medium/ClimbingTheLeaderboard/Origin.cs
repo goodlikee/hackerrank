781639b8088d9b9d1560a23914a573cb3b1951b2
goodlikee <lfhvb0@yandex.ru>
2022-03-11 00:31:43 +0300

    ClimbingTheLeaderboard

---
+++ Tasks/Algorithms/Medium/ClimbingTheLeaderboard/Origin.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace Tasks.Algorithms.Medium.ClimbingTheLeaderboard
+{
+    class Result
+    {
+        /*
+         * Complete the 'climbingLeaderboard' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY ranked
+         *  2. INTEGER_ARRAY player
+         */
+
+        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+        {
+            var rankedS = new SortedList<int, int>(ranked.Distinct().ToDictionary(x => x));
+            var userPositions = new List<int> { };
+            foreach (var value in player)
+            {
+                if (rankedS.IndexOfKey(value) == -1)
+                {
+                    rankedS.Add(value, value);
+                }
+
+                userPositions.Add(rankedS.Count - rankedS.IndexOfKey(value));
+            }
+
+            return userPositions;
+        }
+    }
+
+    class Solution
+    {
+        public static void Main2(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList()
+                .Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+            int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList()
+                .Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+            List<int> result = Result.climbingLeaderboard(ranked, player);
+
+            textWriter.WriteLine(String.Join("\n", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ Tasks/Algorithms/Medium/ClimbingTheLeaderboard/Task.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Tasks.Utils;
+using System.IO;
+
+namespace Tasks.Algorithms.Medium.ClimbingTheLeaderboard
+{
+    public class Task : TestBase<Task>
+    {
+        [Test]
+        public void Test1()
+        {
+            runOneTest();
+        }
+
+        private void runOneTest()
+        {
+            Solution.Main2(null);
+            _streamOutput = StreamHelper.Make(File.OpenRead(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH")));
+
+            var s1 = _streamExpectedOutput.StreamReader.ReadLine();
+            var s2 = _streamOutput.StreamReader.ReadLine();
+            Assert.IsTrue(string.Equals(s1, s2));
+        }
+    }
+}
