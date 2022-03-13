ca0ea3127be31a085c9cb5b72d380821af89054a
goodlikee <lfhvb0@yandex.ru>
2022-03-13 10:48:33 +0300

    runTest

--- Tasks/Algorithms/Easy/DrawingBook/Task.cs
+++ Tasks/Algorithms/Easy/DrawingBook/Task.cs
@@ -9,23 +9,13 @@ namespace Tasks.Algorithms.Easy.DrawingBook
         [Test]
         public void Test1()
         {
-            runOneTest();
+            runTest<Solution>();
         }
 
         [Test]
         public void Test2()
         {
-            runOneTest();
-        }
-
-        private void runOneTest()
-        {
-            Solution.Main2(null);
-            _streamOutput = StreamHelper.Make(File.OpenRead(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH")));
-
-            var s1 = _streamExpectedOutput.StreamReader.ReadLine();
-            var s2 = _streamOutput.StreamReader.ReadLine();
-            Assert.IsTrue(string.Equals(s1, s2));
+            runTest<Solution>();
         }
     }
 }
--- Tasks/Algorithms/Easy/StrongPassword/Task.cs
+++ Tasks/Algorithms/Easy/StrongPassword/Task.cs
@@ -9,12 +9,7 @@ namespace Tasks.Algorithms.Easy.StrongPassword
         [Test]
         public void Test1()
         {
-            Solution.Main2(null);
-            _streamOutput = StreamHelper.Make(File.OpenRead(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH")));
-
-            var s1 = _streamExpectedOutput.StreamReader.ReadLine();
-            var s2 = _streamOutput.StreamReader.ReadLine();
-            Assert.IsTrue(string.Equals(s1, s2));
+            runTest<Solution>();
         }
     }
 }
